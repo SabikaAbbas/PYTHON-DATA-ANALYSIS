ab25f1f2355463c54bbc365dc73b95c2c109d02b
SabikaAbbas <sabika.naqvi95@gmail.com>
2026-08-11 15:03:36 -0600

    check survival rate by gender

--- Titanic_analysis.py
+++ Titanic_analysis.py
@@ -14,3 +14,4 @@ print(df["Cabinknown"].value_counts())
 print(df["Survived"].value_counts())
 survival_rate=df["Survived"].mean()*100
 print(f"overall survival rate: {survival_rate:.2f}%")
+print(df.groupby("Sex")["Survived"].mean().mul(100).round(2))
