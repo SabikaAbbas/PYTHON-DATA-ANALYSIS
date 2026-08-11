7e18a63a274b847e8a4b69029a79f556fc68d424
SabikaAbbas <sabika.naqvi95@gmail.com>
2026-08-11 13:37:34 -0600

    Handling missing Embarked values

--- Titanic_analysis.py
+++ Titanic_analysis.py
@@ -7,6 +7,7 @@ print(df.isnull().sum())
 print(df.duplicated().sum())
 print(df.columns)
 df["Age"]=df["Age"].fillna(df["Age"].median())
+df["Embarked"]=df["Embarked"].fillna(df["Embarked"].mode()[0])
 
 
 
