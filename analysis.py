909dbc21048bca1406f3ab522705fb2611186e81
SabikaAbbas <sabika.naqvi95@gmail.com>
2026-08-11 11:39:52 -0600

    Add Pandas data analysis

--- analysis.py
+++ analysis.py
@@ -1,20 +1,29 @@
 # Python Data Analysis Project
 
-students = {
+import pandas as pd
+
+# Student data
+data = {
     "Name": ["Ali", "Sara", "John", "Ayesha", "David"],
     "Math": [85, 92, 78, 88, 95],
     "Science": [80, 89, 75, 90, 91],
     "English": [82, 94, 80, 87, 93]
 }
 
-print("Student Data:")
-print(students)
+# Create a DataFrame
+df = pd.DataFrame(data)
 
-print("\nMath Scores:")
-print(students["Math"])
+print("Student Data:")
+print(df)
+
+print("\nAverage Scores:")
+print(df[["Math", "Science", "English"]].mean())
 
 print("\nHighest Math Score:")
-print(max(students["Math"]))
+print(df["Math"].max())
 
-print("\nAverage Math Score:")
-print(sum(students["Math"]) / len(students["Math"]))
+print("\nHighest Science Score:")
+print(df["Science"].max())
+
+print("\nHighest English Score:")
+print(df["English"].max())
